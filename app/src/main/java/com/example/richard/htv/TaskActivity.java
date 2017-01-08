7531d5963613122c3811500aa335cc83bb23b829
RLKANG <richrichard2002@yahoo.ca>
2017-01-07 22:20:23 -0500

    Edit 10:20pm

--- app/src/main/java/com/example/richard/htv/TaskActivity.java
+++ app/src/main/java/com/example/richard/htv/TaskActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.CheckBox;
 import android.widget.Toast;
 import android.widget.TextView;
 
@@ -45,26 +46,31 @@ public class TaskActivity extends AppCompatActivity {
                 interpreter.addTask(name, point);
                 if (count == 0) {
                     TextView txtname = (TextView) findViewById(R.id.Task1);
+                    txtname.setVisibility(View.VISIBLE);
                     txtname.setText(name);
                     TextView txtpoint = (TextView) findViewById(R.id.Point1);
                     txtpoint.setText(String.valueOf(point));
                 } else if (count == 1) {
                     TextView txtname = (TextView) findViewById(R.id.Task2);
+                    txtname.setVisibility(View.VISIBLE);
                     txtname.setText(name);
                     TextView txtpoint = (TextView) findViewById(R.id.Point2);
                     txtpoint.setText(String.valueOf(point));
                 } else if (count == 2) {
                     TextView txtname = (TextView) findViewById(R.id.Task3);
+                    txtname.setVisibility(View.VISIBLE);
                     txtname.setText(name);
                     TextView txtpoint = (TextView) findViewById(R.id.Point3);
                     txtpoint.setText(String.valueOf(point));
                 } else if (count == 3) {
                     TextView txtname = (TextView) findViewById(R.id.Task4);
+                    txtname.setVisibility(View.VISIBLE);
                     txtname.setText(name);
                     TextView txtpoint = (TextView) findViewById(R.id.Point4);
                     txtpoint.setText(String.valueOf(point));
                 } else if (count == 4) {
                     TextView txtname = (TextView) findViewById(R.id.Task5);
+                    txtname.setVisibility(View.VISIBLE);
                     txtname.setText(name);
                     TextView txtpoint = (TextView) findViewById(R.id.Point5);
                     txtpoint.setText(String.valueOf(point));
