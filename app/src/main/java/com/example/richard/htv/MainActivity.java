21dd2c15be1856ecf7e1e852aea0d9ac4d07f639
chengyingyue <chengyingyue@outlook.com>
2017-01-07 22:51:23 -0500

    putExtra fixed

--- app/src/main/java/com/example/richard/htv/MainActivity.java
+++ app/src/main/java/com/example/richard/htv/MainActivity.java
@@ -13,10 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try{
-            Intent intent = getIntent();
+        Intent intent = getIntent();
+        if(intent.getExtras().containsKey("interpreter")) {
             interpreter = (Interpreter) intent.getSerializableExtra("interpreter");
-        }catch (Exception e){
+        }else {
             interpreter = new Interpreter();
         }
     }
